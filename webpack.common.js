12316049b45c51b101139e9d617b6eb97518f177
patrickcrocker <patrick.crocker@gmail.com>
2020-05-24 13:52:03 -0500

    split webpack configs out for dev and prod

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require("path")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: "./app/Main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js",
+    
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "app/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", ["@babel/preset-env", { targets: { node: "12" } }]]
+          }
+        }
+      }
+    ]
+  }
+}
