ffc774303f91a3b96dfa1e76a60780c4fe965cfb
patrickcrocker <patrick.crocker@gmail.com>
2020-05-30 15:34:49 -0500

    add ProfilePosts

--- app/Main.js
+++ app/Main.js
@@ -18,6 +18,7 @@ import Terms from "./components/Terms"
 import CreatePost from "./components/CreatePost"
 import ViewSinglePost from "./components/ViewSinglePost"
 import FlashMessages from "./components/FlashMessages"
+import Profile from "./components/Profile"
 
 function Main() {
   const initialState = {
@@ -68,6 +69,9 @@ function Main() {
           <Switch>
             <Route path="/" exact>
               {state.loggedIn ? <Home /> : <HomeGuest />}
+            </Route>
+            <Route path="/profile/:username">
+              <Profile />
             </Route>
             <Route path="/post/:id">
               <ViewSinglePost />
