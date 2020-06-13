9304ee9ddec96ea7fbb323c71777a189f55cb4fd
patrickcrocker <patrick.crocker@gmail.com>
2020-06-13 07:36:33 -0500

    use webpack.config.js and .env

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const currentTask = process.env.npm_lifecycle_event
+const path = require("path")
+const Dotenv = require("dotenv-webpack")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const fse = require("fs-extra")
+
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap("Copy files", function () {
+      fse.copySync("./app/main.css", "./dist/main.css")
+      fse.copySync("./app/Staticfile", "./dist/Staticfile")
+    })
+  }
+}
+
+config = {
+  entry: "./app/Main.js",
+  output: {
+    publicPath: "/",
+    path: path.resolve(__dirname, "app"),
+    filename: "bundled.js"
+  },
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "app/index-template.html",
+      alwaysWriteToDisk: true
+    }),
+    new HtmlWebpackHarddiskPlugin()
+  ],
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", ["@babel/preset-env", { targets: { node: "12" } }]]
+          }
+        }
+      }
+    ]
+  }
+}
+
+if (currentTask == "webpackDev" || currentTask == "dev") {
+  config.devtool = "source-map"
+  config.devServer = {
+    port: 3000,
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    historyApiFallback: { index: "index.html" }
+  }
+}
+
+if (currentTask == "webpackBuild") {
+  config.plugins.push(new CleanWebpackPlugin(), new RunAfterCompile())
+  config.mode = "production"
+  config.output = {
+    publicPath: "/",
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js"
+  }
+}
+
+module.exports = config
